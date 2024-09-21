380150ed2249d98a53f1eabc4849c86b4ba7e91b
songhaechan <bukak2019@naver.com>
2024-09-21 14:15:04 +0900

    init

---
+++ src/test/java/test/rtc/RtcApplicationTests.java
@@ -0,0 +1,13 @@
+package test.rtc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RtcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
