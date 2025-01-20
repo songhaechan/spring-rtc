8214a1880a486b78dbec00184ebdb06eb055052f
songhaechan <bukak2019@naver.com>
2025-01-20 16:31:56 +0900

    feat: logging interceptor

--- src/main/java/test/rtc/config/WebSocketConfig.java
+++ src/main/java/test/rtc/config/WebSocketConfig.java
@@ -17,6 +17,7 @@ public class WebSocketConfig implements WebSocketConfigurer {
     @Override
     public void registerWebSocketHandlers(final WebSocketHandlerRegistry registry) {
         registry.addHandler(chatHandler,"/ws/chat")
+            .addInterceptors(new LoggingHandshakeInterceptor())
             .setAllowedOrigins("*");
     }
 }
