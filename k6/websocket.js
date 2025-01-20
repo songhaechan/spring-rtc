38ecd4dc9a9342e22ecc58a477b072bd9a40c1b8
songhaechan <bukak2019@naver.com>
2025-01-20 15:35:16 +0900

    feat: k6 script

---
+++ k6/websocket.js
@@ -0,0 +1,47 @@
+import { check, sleep } from "k6";
+import http from "k6/http";
+import ws from "k6/ws";
+
+const wsUrl = "ws://34.22.108.61:8080/ws/chat";
+
+export let options = {
+  stages: [
+    { duration: "20s", target: 300 },
+    { duration: "30s", target: 10000 },
+    { duration: "10s", target: 0 },
+  ],
+};
+
+let websocketConnection = null;
+
+function initializeWebSocketConnection() {
+  if (websocketConnection) {
+    return websocketConnection;
+  }
+  const connection = ws.connect(wsUrl, function (socket) {
+    socket.on("open", function () {
+      socket.send("Hello from k6 WebSocket!");
+    });
+
+    socket.on("message", function (message) {});
+
+    socket.on("close", function () {});
+  });
+
+  websocketConnection = connection;
+  return connection;
+}
+
+export default function () {
+  websocketTest();
+  sleep(3);
+}
+
+export function websocketTest() {
+  initializeWebSocketConnection();
+  ws.connect(wsUrl, function (socket) {
+    socket.on("open", function () {
+      socket.send("Hello from k6!");
+    });
+  });
+}
